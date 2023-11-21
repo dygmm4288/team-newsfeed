c8a4938346daa5065aa6300b6ba21f736505974e
chunmingyu <gyu4016@gmail.com>
2023-11-22 01:45:47 +0900

    [ADD] devfiles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
+
+import { useEffect } from 'react';
+import { app } from './firebase/firebase.config';
+
 function App() {
+  useEffect(() => {
+    console.log(app);
+  }, []);
+
   return (
     <BrowserRouter>
       <Router />
