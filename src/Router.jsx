bdea30d9496239045a06a5e94b7d77d845b55c46
Hamin Lee <whereanna7@gmail.com>
2023-11-21 16:43:48 +0900

    [UPDATE]상세페이지 Router 연결

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import MyPage from './pages/MyPage';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="mypage" element={<MyPage />}></Route>
     </Routes>
   );
 }
