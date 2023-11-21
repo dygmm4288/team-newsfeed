d00215354e9185b7176629aaa7f28904cef0deb2
Hamin Lee <whereanna7@gmail.com>
2023-11-21 15:59:28 +0900

    [ADD] MyPage 파일 추가

---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function MyPage() {
+  return (
+    <>
+      {/* Layout */}
+      <div> 
+      </div>
+      <div>
+        <div>
+          <img />
+          <div>
+            아이디 : 
+          </div>
+          <div>
+            <button>홈으로 가기</button>
+            <button>프로필 사진 수정</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MyPage
