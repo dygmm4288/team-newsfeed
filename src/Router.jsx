5a1b10310c4714a0f3998d1275d4500041f195e4
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-21 15:56:50 +0900

    [UPDATE] router.jsx 파일 내 경로 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,7 +4,9 @@ import Home from './pages/Home';
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/my-page" element={<div>my page</div>} />
+      <Route path="/auth" element={<div>login page</div>} />
     </Routes>
   );
 }
