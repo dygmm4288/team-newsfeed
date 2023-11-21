f1a79f2689729e4a301744198c5b11291c0a739b
Hamin Lee <whereanna7@gmail.com>
2023-11-22 01:38:31 +0900

    [UPDATE] 프로필 수정하기 취소 기능 추가

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,21 +1,33 @@
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { storage } from '../firebase/firebase.config';
 
 function FileUpload({setImgUrl,userId}) {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFileIsFilled, setSelectedFileIsFilled] = useState(false);
+
+  useEffect(() => {
+    setSelectedFile(null);
+  },[selectedFileIsFilled]);
+
+  // const cancelProfileSetting = () => {
+  //   setSelectedFileIsFilled(false);
+  //   console.log("?",selectedFileIsFilled);
+  // }
 
   const handleFileSelect = (event) => {
     setSelectedFile(event.target.files[0]);
+    setSelectedFileIsFilled(true);
   }
 
   const handleUpload = async () => {
     const imageRef = ref(storage, `profile/${userId}`);
     if(window.confirm("프로필 사진을 변경하시겠습니까?")){
       await uploadBytes(imageRef, selectedFile);
+      setSelectedFileIsFilled(true);
       alert("변경사항이 저장되었습니다.");
-      setSelectedFile(""); // input 필드 초기화 안됨
+      setSelectedFileIsFilled(false);
     } else {
       return
     }
@@ -26,10 +38,20 @@ function FileUpload({setImgUrl,userId}) {
     setImgUrl(downloadURL);
   };
 
+
+
   return (
     <StImageInputContainer>
-      <StImageInput type="file" onChange={handleFileSelect}/>
-      <StButton onClick={handleUpload}>변경사항 저장</StButton>
+      {selectedFileIsFilled === false ?
+        (<StProfileEditButton onClick={()=> setSelectedFileIsFilled(true)}>프로필 사진<br />수정하기</StProfileEditButton>)
+      : (<StImageInputAfterContainer>
+          <StImageInput type="file" onChange={handleFileSelect}/>
+          <StButton onClick={()=>setSelectedFileIsFilled(false)}>취소하기</StButton>
+          <StButton onClick={handleUpload}>변경사항 저장</StButton>
+        </StImageInputAfterContainer>
+        )
+      }
+
     </StImageInputContainer>
   )
 }
@@ -40,14 +62,31 @@ const StImageInputContainer = styled.div`
   border: 1px solid red;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   width: 80%;
+  gap: 10px;
+`;
+
+const StProfileEditButton = styled.button`
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+const StImageInputAfterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
   gap: 10px;
 `;
 
 const StImageInput = styled.input`
   cursor: pointer;
+  width: 100%;
 `;
 
 const StButton = styled.button`
   cursor: pointer;
+  width: 100%;
 `;
