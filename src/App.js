4556eb7178442fdebc7310a7b78af7744a9a38ba
Hamin Lee <whereanna7@gmail.com>
2023-11-22 12:29:47 +0900

    [FIX] homepage 브랜치와 머지 후, 랜더링 에러 해결

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 import Router from './Router';
+import { app } from './firebase/firebase.config';
 
 function App() {
   console.log(app);
