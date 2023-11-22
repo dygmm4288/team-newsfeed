a37717e8aaa556de571e04a278b88cf279e473f0
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-22 15:19:20 +0900

    [ADD] auth context 내 userInfo 객체, setter 함수(nickname, profile img url) 추가

--- src/contexts/auth.context.js
+++ src/contexts/auth.context.js
@@ -1,7 +1,8 @@
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
-  signOut
+  signOut,
+  updateProfile
 } from 'firebase/auth';
 import { createContext, useContext, useEffect, useState } from 'react';
 import { auth } from '../firebase/firebase.config';
@@ -11,6 +12,8 @@ const initialState = {
   user: auth.currentUser,
   signInWithEmail: (email, password) => {},
   signOutUser: () => {},
+  setUserNickname: (nickname) => {},
+  setUserProfileImgUrl: (profileImgUrl) => {},
   error: null
 };
 // context 생성
@@ -31,6 +34,13 @@ export const AuthProvider = ({ children }) => {
     signOut(auth);
   };
 
+  const setUserNickname = (nickname) => {
+    updateProfileBy({ displayName: nickname });
+  };
+  const setUserProfileImgUrl = (profileImgUrl) => {
+    updateProfile({ photoURL: profileImgUrl });
+  };
+
   useEffect(() => {
     onAuthStateChanged(auth, (authUser) => {
       if (authUser) {
@@ -41,9 +51,39 @@ export const AuthProvider = ({ children }) => {
     });
   }, []);
 
-  const value = { user, signInWithEmail, error, signOutUser };
+  const userInfo = user
+    ? {
+        nickname: user.displayName,
+        email: user.email,
+        profileImgUrl: user.photoURL
+      }
+    : null;
+
+  const value = {
+    userInfo,
+    signInWithEmail,
+    error,
+    signOutUser,
+    setUserNickname,
+    setUserProfileImgUrl
+  };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => useContext(AuthContext);
+
+const updateProfileBy = (updatedValue) => {
+  if (!auth.currentUser) return;
+  updateProfile(auth.currentUser, updatedValue)
+    .then(() => {
+      console.log('update success');
+    })
+    .catch((err) => {
+      console.error('update failed');
+      console.error(err);
+    })
+    .finally(() => {
+      console.log('update logic progressed');
+    });
+};
