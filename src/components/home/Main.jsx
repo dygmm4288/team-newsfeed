e84d8df46343aaa68f72345f09e18b8b9b627e21
chunmingyu <gyu4016@gmail.com>
2023-11-23 20:51:39 +0900

    [ADD] CRUD with no ERROR

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,68 +1,21 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-<<<<<<< HEAD
-import Post from './Post';
-import { addDoc, collection, getDocs } from 'firebase/firestore';
-import { db } from '../../firebase/firebase.config';
-import { v4 as uuidv4 } from 'uuid';
-=======
 import { usePost } from '../../contexts/post.context';
 import Post from './Posts';
->>>>>>> 03a0b4c6ad04e0d7cbc0c9660122d72941dfb473
 
 function Main() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   // const { category } = useParams();
   const [category, setCategory] = useState('전체보기');
-<<<<<<< HEAD
-
-  // data get (가져오기)
-  useEffect(() => {
-    const fetchData = async () => {
-      const querySnapshot = await getDocs(collection(db, 'posts'));
-      const fetchedPosts = [];
-      querySnapshot.forEach((doc) => {
-        fetchedPosts.push({ id: doc.id, ...doc.data() });
-        console.log(fetchedPosts);
-      });
-      setPosts(fetchedPosts);
-    };
-    fetchData();
-  }, []);
-
-=======
   const { posts, createPost } = usePost();
   console.log(posts);
->>>>>>> 03a0b4c6ad04e0d7cbc0c9660122d72941dfb473
   // data get (추가하기)
-  const handleAddPost = async (event) => {
+  const hanbleAddPost = async (event) => {
     event.preventDefault();
 
     if (title.trim() && content.trim()) {
-<<<<<<< HEAD
-      const newPost = {
-        // nickname : 회원가입 후 작성한 닉네임 값
-        //profileImg : 회원가입시 등록한 이미지 값
-        // id: uuidv4(),
-        title: title,
-        content: content,
-        createdAt: new Date().toLocaleString(),
-        category: category
-        //updatedAt은 수정쪽에서 건들기
-        //likeCount,review는 추가기능
-      };
-
-      setPosts([newPost, ...posts]);
-
-      //Firestore에서 'posts'컬렉션에 대한 참조 생성하기
-      const collectionRef = collection(db, 'posts');
-      // 'posts' 컬렉션에 newPost 문서를 추가
-      await addDoc(collectionRef, newPost);
-
-=======
       createPost({ title, content, category });
->>>>>>> 03a0b4c6ad04e0d7cbc0c9660122d72941dfb473
       setTitle('');
       setContent('');
     } else {
@@ -71,7 +24,7 @@ function Main() {
   };
   return (
     <StContainer>
-      <form onSubmit={(event) => handleAddPost(event)}>
+      <form onSubmit={(event) => hanbleAddPost(event)}>
         <input
           type="text"
           value={title}
@@ -107,16 +60,7 @@ function Main() {
         <button type="submit">추가</button>
       </form>
       <StPostBox>
-<<<<<<< HEAD
-        <Post
-          title={title}
-          content={content}
-          posts={posts}
-          setPosts={setPosts}
-        />
-=======
         <Post />
->>>>>>> 03a0b4c6ad04e0d7cbc0c9660122d72941dfb473
       </StPostBox>
     </StContainer>
   );
