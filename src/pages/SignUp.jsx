3e26b8747520e876f5ed4fa967bb66609903c5c1
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-25 00:28:19 +0900

    [UPDATE] 회원 가입 시 에러 핸들링

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -22,10 +22,27 @@ export default function SignUp() {
 
   const signUp = (event) => {
     event.preventDefault();
-    signUpByEmail(email, password, nickname).then(() => {
-      alert('회원가입에 성공했습니다.');
-      navigate('/');
-    });
+    signUpByEmail(email, password, nickname)
+      .then(() => {
+        alert('회원가입에 성공했습니다.');
+        navigate('/');
+      })
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/email-already-in-use':
+            return alert('이미 사용 중인 이메일입니다.');
+          case 'auth/internal-error':
+            return alert('잘못된 요청입니다.');
+          case 'auth/weak-password':
+            return alert('비밀번호는 6글자 이상이어야 합니다.');
+          case 'auth/network-request-failed':
+            return alert('네트워크 연결에 실패 하였습니다.');
+          case 'auth/invalid-email':
+            return alert('잘못된 이메일 형식입니다.');
+          default:
+            return alert('회원가입에 실패했습니다.');
+        }
+      });
   };
 
   return (
