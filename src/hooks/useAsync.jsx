531672448acea0781b45612a60dcd8bf1bbdc190
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-26 13:34:19 +0900

    [ADD] useAsync 훅 추가

--- src/hooks/useAsync.jsx
+++ src/hooks/useAsync.jsx
@@ -3,22 +3,21 @@ import { useState } from 'react';
 export default function useAsync() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
 
-  const executeAsyncLogic = (asyncLogic) => {
-    setError(null);
+  const executeAsyncLogic = async (taskName, asyncApi, options) => {
     setIsLoading(true);
-    asyncLogic()
-      .then((response) => {
-        setData(response);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    setError(null);
+    try {
+      const result = await asyncApi();
+      if (options?.asyncTask) await options.asyncTask(result);
+    } catch (err) {
+      setError(err);
+      console.error(`An Error occurred while ${taskName}`);
+    } finally {
+      setIsLoading(false);
+      if (options?.finallyTask) await options.finallyTask();
+    }
   };
 
-  return [isLoading, data, executeAsyncLogic, error];
+  return [isLoading, executeAsyncLogic, error];
 }
