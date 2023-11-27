f97f853811007172c8cdd80fc209a5b7352550f3
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-28 07:59:39 +0900

    [REFACTOR] browser router 용도에 맞게 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Auth from './pages/Auth';
 import Home from './pages/Home';
@@ -7,13 +7,15 @@ import SignUp from './pages/SignUp';
 
 export default function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="my-page" element={<MyPage />}></Route>
-        <Route exact path="auth" element={<Auth />} />
-        <Route exact path="sign-up" element={<SignUp />}></Route>
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="my-page" element={<MyPage />}></Route>
+          <Route exact path="auth" element={<Auth />} />
+          <Route exact path="sign-up" element={<SignUp />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
